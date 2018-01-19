b0567988df61780a057bd56ee24cd2b5e702c8ca
jczetta <jczetta@gmail.com>
2018-01-18 23:26:34 -0500

    Solution

--- examplelec3.py
+++ examplelec3.py
@@ -4,13 +4,19 @@ app = Flask(__name__)
 
 @app.route('/oranges')
 def lemons():
-    title = "My Ice Cream Form"
-    # Add code -- what type should options hold?
+    title_var = "My Ice Cream Form"
+    options = ["Vanilla", "Chocolate","Pistachio", "So many others!!!!"]
     return render_template('seeform.html',title=title_var, lst_stuff=options)
 
 @app.route('/apples')
 def plants():
-    ## Add code here
+    if request.method == "GET":
+        name = request.args.get('name')
+        name_len = len(name)
+        flavor_options = []
+        for k in request.args:
+            if k != "name":
+                flavor_options.append(request.args[k])
     return render_template('results.html',flavors=flavor_options, name_len=name_len, name=name)
 
 
